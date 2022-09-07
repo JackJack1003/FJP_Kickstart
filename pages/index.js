1e170d4c7e56d140a6be656c58830f31c51162fa
Jack <jackcoetzer23@gmail.com>
2022-09-07 13:36:44 +0200

    password hash

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,30 @@ import Layout from '../components/Layout';
 import { Link } from '../routes';
 import { Router } from '../routes';
 import web3 from '../ethereum/web3';
+var crypto = require('crypto');
 
 class Login extends Component {
+  state = {
+    password: '',
+    newPassword: '',
+    salt: '',
+  };
   callCreate = async () => {
     console.log('hy het geclick');
     try {
-      const accounts = await web3.eth.getAccounts();
-      await factory.methods.createCampaign('0').send({ from: accounts[0] });
+      // const accounts = await web3.eth.getAccounts();
+      // await factory.methods.createCampaign('0').send({ from: accounts[0] });
 
-      Router.pushRoute('/home');
+      // //Router.pushRoute('/home');
+      console.log(this.state.password);
+      await this.setState({ salt: crypto.randomBytes(16).toString('hex') });
+      await this.setState({
+        newPassword: crypto
+          .pbkdf2Sync(this.state.password, this.state.salt, 1000, 64, `sha512`)
+          .toString(`hex`),
+      });
+      console.log(this.state.salt);
+      console.log(this.state.newPassword);
     } catch (err) {
       console.log(err);
     }
@@ -27,6 +42,10 @@ class Login extends Component {
             <button>Login</button>
           </a>
         </Link>
+        <input
+          value={this.state.password}
+          onChange={(e) => this.setState({ password: e.target.value })}
+        />
 
         <button onClick={() => this.callCreate()}>Sign up</button>
       </div>
